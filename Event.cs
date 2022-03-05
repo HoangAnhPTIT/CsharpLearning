a74470000cd67fa968b1a7228a62f663a50b1914
AnhCH <anhch@tuivietnam.com>
2022-03-05 22:53:28 +0700

    Remove ignored files

--- Event.cs
+++ Event.cs
@@ -1,75 +1,69 @@
 ﻿using System;
-using System.Collections;
 
 namespace ConsoleApp
 {
-  class Event
-  {
-    static void Main(string[] args)
+    internal class Event
     {
-      HocSinh hs = new HocSinh();
-      hs.NameChanged += Hs_NameChanged;
+        //private static void Main(string[] args)
+        //{
+        //    HocSinh hs = new HocSinh();
+        //    hs.NameChanged += Hs_NameChanged;
 
-      hs.Name = "A";
-      hs.Name = "B";
+        //    hs.Name = "A";
+        //    hs.Name = "B";
+        //}
 
+        private static void Hs_NameChanged(object sender, NameChangedEventArgs e)
+        {
+            Console.WriteLine("Ten thay doi: " + e.Name);
+        }
     }
 
-    private static void Hs_NameChanged(object sender, NameChangedEventArgs e)
+    internal class HocSinh
     {
-      Console.WriteLine("Ten thay doi: " + e.Name);
+        private string _Name;
 
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                _Name = value;
+                OnNameChanged(value);
+            }
+        }
+
+        private event EventHandler<NameChangedEventArgs> _NameChanged;
+
+        public event EventHandler<NameChangedEventArgs> NameChanged
+        {
+            add
+            {
+                _NameChanged += value;
+            }
+
+            remove
+            {
+                _NameChanged -= value;
+            }
+        }
+
+        public void OnNameChanged(string name)
+        {
+            if (_NameChanged != null)
+            {
+                _NameChanged(this, new NameChangedEventArgs(name));
+            }
+        }
     }
 
-  }
-
-  class HocSinh
-  {
-    private string _Name;
-
-    public string Name
+    public class NameChangedEventArgs : EventArgs
     {
-      get => _Name;
-      set
-      {
-        _Name = value;
-        OnNameChanged(value);
-      }
+        public string Name { get; set; }
+
+        public NameChangedEventArgs(string name)
+        {
+            Name = name;
+        }
     }
-
-    private event EventHandler<NameChangedEventArgs> _NameChanged;
-
-    public event EventHandler<NameChangedEventArgs> NameChanged
-    {
-      add
-      {
-        _NameChanged += value;
-      }
-
-      remove
-      {
-        _NameChanged -= value;
-      }
-    }
-
-    public void OnNameChanged(string name)
-    {
-      if (_NameChanged != null)
-      {
-        _NameChanged(this, new NameChangedEventArgs(name));
-      }
-    }
-
-
-  }
-
-  public class NameChangedEventArgs : EventArgs
-  {
-    public string Name { get; set; }
-
-    public NameChangedEventArgs(string name)
-    {
-      Name = name;
-    }
-  }
 }
---
+++ MultiThreading.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class MultiThreading
+    {
+        //static void Main(string[] args)
+        //{
+        //    DemoThread();
+        //    DemoThread();
+        //    DemoThread();
+        //}
+
+        public static void DemoThread()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+}
