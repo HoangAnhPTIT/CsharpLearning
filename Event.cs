8abe4920478cd5763eadc974b418746468d80e77
AnhCH <anhch@tuivietnam.com>
2022-03-05 20:50:37 +0700

    event handler

---
+++ Event.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp
+{
+  class Event
+  {
+    static void Main(string[] args)
+    {
+      HocSinh hs = new HocSinh();
+      hs.NameChanged += Hs_NameChanged;
+
+      hs.Name = "A";
+      hs.Name = "B";
+
+    }
+
+    private static void Hs_NameChanged(object sender, NameChangedEventArgs e)
+    {
+      Console.WriteLine("Ten thay doi: " + e.Name);
+
+    }
+
+  }
+
+  class HocSinh
+  {
+    private string _Name;
+
+    public string Name
+    {
+      get => _Name;
+      set
+      {
+        _Name = value;
+        OnNameChanged(value);
+      }
+    }
+
+    private event EventHandler<NameChangedEventArgs> _NameChanged;
+
+    public event EventHandler<NameChangedEventArgs> NameChanged
+    {
+      add
+      {
+        _NameChanged += value;
+      }
+
+      remove
+      {
+        _NameChanged -= value;
+      }
+    }
+
+    public void OnNameChanged(string name)
+    {
+      if (_NameChanged != null)
+      {
+        _NameChanged(this, new NameChangedEventArgs(name));
+      }
+    }
+
+
+  }
+
+  public class NameChangedEventArgs : EventArgs
+  {
+    public string Name { get; set; }
+
+    public NameChangedEventArgs(string name)
+    {
+      Name = name;
+    }
+  }
+}
